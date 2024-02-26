48a636a77acdaba194eda8cd01cf3b76889a0908
Maria Krulikovskaya <mari.krulikovskaya@mail.ru>
2024-02-26 22:05:20 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ul>
+         <li>html</li>
+         <li>css</li>
+         <li>js</li>
+         <li>react</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;

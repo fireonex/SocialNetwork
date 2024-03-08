afd903c296ac98fa58d4c6fd28732a7db102b0f1
fireonex <mari.krulikovskaya@mail.ru>
2024-03-08 13:26:28 +0300

    third commit

--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Header = () => {
-    return (<div>
-        <a href="#">Номе</a>
-        <a href="#">New Feed</a>
-        <a href="#">Messages</a>
-    </div>);
-}
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAHXPluq6GtTRPDIHRv5kJPy86uFjp5sO7hg&usqp=CAU'}
+                alt={'logo'}/>
+        </header>
+    )
+}

80c9f2d5f677f2913ae16f1fa867571b40f44b47
fireonex <mari.krulikovskaya@mail.ru>
2024-03-25 20:27:35 +0300

    sixth commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/news/News";
+import {Music} from "./components/music/Music";
 
 
 function App () {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
 
-            <div className='app-wrapper-content'>
-                <Profile/>
+                <div className='app-wrapper-content'>
+                    <Route path={'/profile'} render={() => <Profile/>}/>
+                    <Route path={'/dialogs'} render={() => <Dialogs/>}/>
+                    <Route path={'/news'} render={() => <News/>}/>
+                    <Route path={'/music'} render={() => <Music/>}/>
+                </div>
+
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,9 +1,45 @@
-import s from './Dialogs.module.css';
+import styled from "styled-components";
+
 
 export const Dialogs = () => {
     return (
-        <div>
-
-        </div>
+        <StyledDialogs>
+            <DialogsItems>
+                <Dialog>Sam</Dialog>
+                <Dialog>John</Dialog>
+                <Dialog>Sarah</Dialog>
+                <Dialog>Tom</Dialog>
+                <Dialog>Rebecca</Dialog>
+            </DialogsItems>
+            <Messages>
+                <Message>Hello</Message>
+                <Message>What's up bro??</Message>
+                <Message>I love you</Message>
+                <Message>Let's go to restaurant</Message>
+            </Messages>
+        </StyledDialogs>
     )
 }
+
+
+const StyledDialogs = styled.section`
+    display: grid;
+    grid-template-columns: 2fr 10fr;
+`
+
+const DialogsItems = styled.div`
+
+`
+
+const Dialog = styled.div`
+    padding: 10px;
+    border: 1px solid #0c3306;
+`
+
+const Messages = styled.div`
+
+`
+
+const Message = styled.div`
+    padding: 10px;
+`
